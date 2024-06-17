adda081ab85b75bf09ba5edaa4f27a872260bbc2
brightnc <bright.siwat@hotmail.com>
2024-06-17 19:31:50 +0700

    final table

--- table-js.js
+++ table-js.js
@@ -1,57 +1,97 @@
-function handleSubmit(e){
-    e.preventDefault();
-    const number_input = document.getElementById("input_number").value
-    const min_input =  document.getElementById("input_min").value
-    const max_input =  document.getElementById("input_max").value
+function handleSubmit(event) {
+  event.preventDefault();
+  const number_input = document.getElementById("input_number").value;
+  const min_input = document.getElementById("input_min").value;
+  const max_input = document.getElementById("input_max").value;
 
-    if(number_input == "" || min_input == "" || max_input == ""){
-        alert("input can not be empty");
-        return
-    }
+  if (number_input == "" || min_input == "" || max_input == "") {
+    alert("input can not be empty");
+    return;
+  }
 
-    if(number_input != 5){
-        alert("number must be 5!")
-        return
-    }
+  if (number_input != 5) {
+    alert("number must be 5!");
+    return;
+  }
 
-    if(min_input > 20 || min_input < 10){
-        alert("min number is between 10-20.")
-        return
-    }
-    
-    if(max_input > 100){
-        alert("max number must not exceed 100.")
-        return
-    }
+  if (min_input > 20 || min_input < 10) {
+    alert("min number is between 10-20.");
+    return;
+  }
 
-    const arr = randomNumArr(min_input,max_input,number_input)
-    const t = document.getElementById("myTable")
-    for(i=0; i < arr.length;i++){
-        const tr = document.createElement("tr")
-        tr.setAttribute("id", "MyTr");
-        t.appendChild(tr)
-        const td = document.createElement("TD"); 
-        const num = document.createTextNode(arr[0]); 
-        td.appendChild(num); 
-        document.getElementById("MyTr").appendChild(td);
-    }
+  if (max_input > 100) {
+    alert("max number must not exceed 100.");
+    return;
+  }
 
+  const arr = randomNumArr(min_input, max_input, number_input);
+  const resultContainer = document.getElementById("day6");
+  const existingTable = document.getElementById("resultTable");
+
+  // Clear previous table
+  if (existingTable) {
+    resultContainer.removeChild(existingTable);
+  }
+
+  // Create new table
+  const table = document.createElement("table");
+  table.setAttribute("id", "resultTable");
+  table.classList.add("w-1/2", "bg-white", "border", "text-center", "mx-auto");
+  resultContainer.appendChild(table);
+
+  // Create table header
+  const trHead = document.createElement("tr");
+  trHead.classList.add("bg-gray-800", "text-white");
+  table.appendChild(trHead);
+
+  const thNo = document.createElement("th");
+  const thNum = document.createElement("th");
+
+  thNo.textContent = "No";
+  thNum.textContent = "Number";
+  trHead.appendChild(thNo);
+  trHead.appendChild(thNum);
+  for (i = 0; i < arr.length; i++) {
+    let count = i + 1;
+    let trIdName = "tr" + count;
+    let tdNoIdName = "tdNo" + count;
+    let tdNumIdName = "tdNum" + count;
+
+    const tr = document.createElement("tr");
+    tr.setAttribute("id", trIdName);
+    table.appendChild(tr);
+
+    const tdNo = document.createElement("td");
+    tdNo.setAttribute("id", tdNoIdName);
+    tdNo.classList.add("border", "px-4", "py-2");
+    const tdNum = document.createElement("td");
+    tdNum.setAttribute("id", tdNumIdName);
+    tdNum.classList.add("border", "px-4", "py-2");
+
+    const num = document.createTextNode(arr[i]);
+    const no = document.createTextNode(count);
+    tdNo.appendChild(no);
+    tdNum.appendChild(num);
+    document.getElementById(trIdName).appendChild(tdNo);
+    document.getElementById(trIdName).appendChild(tdNum);
+  }
+  return;
 }
 
-function randomNumArr(min=10,max=100,round=5){
-    const resultArr = [];
-    for(i = 1;i <= round;i++){
-        let rand = Math.floor(Math.random() * max) + 1;
-        if(rand < min){
-            rand = min
-        }
-
-        if(rand > max){
-            rand = max
-        }
-
-        resultArr.push(rand)
+function randomNumArr(min = 10, max = 100, round = 5) {
+  const resultArr = [];
+  for (i = 1; i <= round; i++) {
+    let rand = Math.floor(Math.random() * max) + 1;
+    if (rand < min) {
+      rand = min;
     }
 
-    return resultArr
+    if (rand > max) {
+      rand = max;
+    }
+
+    resultArr.push(rand);
+  }
+
+  return resultArr;
 }
--- table.php
+++ table.php
@@ -1,39 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
     <script src="table-js.js"></script>
-</head>
+    <script src="https://cdn.tailwindcss.com"></script>
+  </head>
 
-<body>
-    <form action="">
-        <div>
-            <label for="input_number">Number</label>
-            <input type="text" name="input_number" id="input_number">
+  <body>
+    <div class="max-w-xs mx-auto my-10 border rounded">
+      <form action="" class="px-8 pt-6 pb-8">
+        <div class="mb-4">
+          <label for="input_number" class="block mb-2">Number</label>
+          <input
+            type="text"
+            name="input_number"
+            id="input_number"
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"
+          />
         </div>
-        <div>
-            <label for="input_min">Min</label>
-            <input type="text" name="input_min" id="input_min">
+        <div class="mb-4">
+          <label for="input_min" class="block mb-2">Min</label>
+          <input
+            type="text"
+            name="input_min"
+            id="input_min"
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"
+          />
         </div>
-        <div>
-            <label for="input_max">Max</label>
-            <input type="text" name="input_max" id="input_max">
+        <div class="mb-4">
+          <label for="input_max" class="block mb-2">Max</label>
+          <input
+            type="text"
+            name="input_max"
+            id="input_max"
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"
+          />
         </div>
-        <button type="button" onclick="handleSubmit(e);">submit</button>
-    </form>
-
-    <div class="day6">
-        <table id="myTable">
-            <tr>
-                <th>No</th>
-                <th>Number</th>
-            </tr>
-
-        </table>
+        <button
+          class="py-2 px-4 rounded border bg-blue-500 font-bold"
+          type="button"
+          onclick="handleSubmit(event);"
+        >
+          submit
+        </button>
+      </form>
     </div>
-</body>
 
+    <div id="day6" class="max-w-xs mx-auto"></div>
+  </body>
 </html>
